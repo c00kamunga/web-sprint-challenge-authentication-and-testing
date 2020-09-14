90dd55d6b72975b5638d14a325af886aab8dc03d
barinder singh <tsothalanti2@yahoo.com>
2020-09-14 16:58:44 -0700

    created an endpoint for registering a new user using bcrypt hash to hash the password upon creation

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,29 @@
+const Users = require('./auth-model')
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const router = require('express').Router();
+
 
 router.post('/register', (req, res) => {
   // implement registration
+  try {
+    const { username, password } = req.body
+    const user = await Users.findBy({ username }).first()
+
+    if (user) {
+      return res.status(409).json({
+        message: "Username is already taken"
+      })
+    }
+    const newUser = await Users.add({
+      username,
+      password: await bcrypt.hash(password, 13)
+    })
+    res.status(201).json(newUser)
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.post('/login', (req, res) => {
